a706760d7c9728765fdb639c062605109eb2c07d
lihe <lihe@192.168.1.163>
2016-02-26 09:49:34 +0800

    img

--- super_IM_server/application/config/config.php
+++ super_IM_server/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.150:8888/';
+$config['base_url'] = 'http://192.168.1.163:8888/';
 
 /*
 |--------------------------------------------------------------------------
